72ce29d5667528f01042e46d48a17653586285de
Tyler Marefke <Tyler Marefke@DESKTOP-QKV0732>
2021-11-17 21:30:15 -0600

    Added methods in TicTacToe and created Coordinates to use in Minimax

--- src/application/Setup.java
+++ src/application/Setup.java
@@ -200,6 +200,16 @@ public class Setup extends BorderPane {
 			gameText.setText("Draw!");
 			isWin = true;
 		}
+		
+		TTT.changePlayer();
+		
+		if(TTT.getCurrentPlayer() == 'X')
+			computerMoves(true);
+		else
+			computerMoves(false);
+		
+		TTT.changePlayer();
+		
 		/*
 		 * Disables buttons on TicTacToe board and reveals the restart button
 		 */
@@ -230,5 +240,138 @@ public class Setup extends BorderPane {
 		restart.setVisible(false);
 		gameText.setText("");
 		isWin = false;
+	}
+	
+	/**
+	 * Minimax algorithm: This algorithm determines the optimal move for the current player, which
+	 * 		assumes that the other player is playing optimally
+	 * @param manipulatingBoard - a copy of the game board
+	 * @param xLocation - the last x location placed in the board
+	 * @param yLocation - the last y location placed in the board
+	 * @param maximizingPlayer - determines if the current player is maximizing or minimizing
+	 * @param alpha - the maximizing indicator for alpha-beta pruning
+	 * @param beta - the minimizing indicator for alpha-beta pruning
+	 * @return a maximizing or minimizing value
+	 */
+	private int miniMaxAlg(TicTacToe manipulatingBoard, int xLocation, int yLocation, boolean maximizingPlayer, int alpha, int beta) {
+		//Runs method to check if a winner has been determined or the board has been filled
+		int winnerOrGameEnd = manipulatingBoard.isWinner(xLocation, yLocation);
+		//Returns the static value of the current board state
+		if(winnerOrGameEnd >= -1 && winnerOrGameEnd <= 1)
+			return winnerOrGameEnd;
+		
+		//Runs if the current player is maximizing
+		if(maximizingPlayer) {
+			//Sets the best move to a low count
+			int bestMax = -1000;
+			
+			//Loops through available moves in the board
+			for(int i = 0; i < manipulatingBoard.getNumberOfOpenSlots(); i++) {
+				
+				//Sets the maximizing player to be X
+				manipulatingBoard.setCurrentPlayer('X');
+				Coordinate current = manipulatingBoard.getCoordinates().get(i);
+				int tempX = current.getxLocation(), tempY = current.getyLocation();
+				manipulatingBoard.placeMarkerInBoard(tempX, tempY);
+				
+				//Runs the minimax alg with minimizing player's turn and saves the maximum result
+				bestMax = Math.max(bestMax, miniMaxAlg(manipulatingBoard, tempX, tempY, false, alpha, beta));
+				
+				//Calculates the new alpha for pruning
+				alpha = Math.max(alpha, bestMax);
+				
+				//removes the last placed marker from the board
+				manipulatingBoard.removeMarkerFromBoard(tempX, tempY, i);
+				
+				//Prunes branches if a better min exists
+				if(beta <= alpha)
+					break;
+			}
+			return bestMax;
+		}else {
+			//Sets the best move to a high count
+			int bestMin = 1000;
+			
+			//Loops through the available moves in the board
+			for(int i = 0; i < manipulatingBoard.getNumberOfOpenSlots(); i++) {
+				
+				//Sets the minimizing player to be O
+				manipulatingBoard.setCurrentPlayer('O');
+				
+				//Stores the current coordinate from the available coordinates
+				Coordinate current = manipulatingBoard.getCoordinates().get(i);
+				int tempX = current.getxLocation(), tempY = current.getyLocation();
+				manipulatingBoard.placeMarkerInBoard(tempX, tempY);
+				
+				//Runs the minimax alg with maximizing player's turn and saves the minimum result
+				bestMin = Math.min(bestMin, miniMaxAlg(manipulatingBoard, tempX, tempY, true, alpha, beta));
+				
+				//Calculates the new beta for pruning
+				beta = Math.min(beta, bestMin);
+				
+				//Removes the last placed marker from the board
+				manipulatingBoard.removeMarkerFromBoard(tempX, tempY, i);
+				
+				//Prunes branches if a better max exists
+				if(beta <= alpha)
+					break;
+			}
+			return bestMin;
+		}
+	}
+	
+	/**
+	 * Determines what the most optimal move is for the computer player and places it into the board
+	 * @param maximizingPlayer - determines whether the computer is maximizing or not
+	 */
+	private void computerMoves(boolean maximizingPlayer) {
+		TicTacToe manipulatingBoard = new TicTacToe(TTT);
+		int xLocation = -1, yLocation = -1, best = 0;
+		//Sets the value of best based on whether the computer is trying to minimize or maximize
+		if(maximizingPlayer)
+			best = -1000;
+		else
+			best = 1000;
+		
+		//Loops through the possible options for the computer player
+		for (int i = 0; i < manipulatingBoard.getNumberOfOpenSlots(); i++) {
+			//Sets current player to O or X depending on if they are minimizing or maximizing
+			if(maximizingPlayer)
+				manipulatingBoard.setCurrentPlayer('X');
+			else
+				manipulatingBoard.setCurrentPlayer('O');
+
+			//Stores the current coordinates to test in the game
+			Coordinate current = manipulatingBoard.getCoordinates().get(i);
+			manipulatingBoard.placeMarkerInBoard(current.getxLocation(), current.getyLocation());
+			
+			//Checks if the move is optimal by running through the minimax algorithm
+			int miniMaxVal = miniMaxAlg(manipulatingBoard, current.getxLocation(), current.getyLocation(), !maximizingPlayer, -1000, 1000);
+			
+			//Changes players most optimal move based on current move
+			if(maximizingPlayer) {
+				if(miniMaxVal > best) {
+					best = miniMaxVal;
+					xLocation = current.getxLocation();
+					yLocation = current.getyLocation();
+				}
+			}else {
+				if(miniMaxVal < best) {
+					best = miniMaxVal;
+					xLocation = current.getxLocation();
+					yLocation = current.getyLocation();
+				}
+			}
+			
+			//Removes marker from board in order to test next marker
+			manipulatingBoard.removeMarkerFromBoard(current.getxLocation(), current.getyLocation(), i);
+		}
+		
+		//Places the best move for the computer into the board
+		TTT.placeMarkerInBoard(xLocation, yLocation);
+		
+		//Prints board the placement for testing purposes
+		System.out.println(TTT);
+		System.out.println(xLocation + " " + yLocation);
 	}
 }
